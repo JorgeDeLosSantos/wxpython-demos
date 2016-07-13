c3c8d50ad1311c671bf5646e0acd729dbaaf3432
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-07-13 12:04:56 -0500

    Updating MiniEditor.py

--- advanced/MiniEditor.py
+++ advanced/MiniEditor.py
@@ -13,137 +13,138 @@ import os
 class MiniEditor(wx.Frame):
 	def __init__(self,parent):
 		wx.Frame.__init__(self,parent,title="MiniEditor 0.1.0",size=(600,400))
-		self.icono = wx.Icon('img/editor_icono.png')
+		self.icono = wx.Icon('img/editor_icon.png')
 		self.SetIcon(self.icono)
-		self.archivo = 'untitled.txt'
+		self.this_file = 'untitled.txt' # Default file name
 		
 		# Sizer
 		sz = wx.BoxSizer(wx.VERTICAL)
 		
 		# Editor
 		self.editor = wx.TextCtrl(self, -1, u"", style=wx.TE_MULTILINE)
-		self.configurarEditor()
+		self.initEditor()
 		
-		# Crear barra de menu
+		# Create menu bar
 		self.crearMenu()
 		
-		# Agregar al sizer
+		# Add to sizer
 		sz.Add(self.editor, 1, wx.EXPAND|wx.ALL,)
 		self.SetSizer(sz)
 		
+        # Centre and show
 		self.Centre(True)
 		self.Show()
 		
 		
-	def configurarEditor(self):
+	def initEditor(self):
 		"""
-		Configura las características iniciales del editor
+		Set initial properties
 		"""
-		self.fuente=wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL)
-		self.fuente.SetFaceName("Courier New")
-		self.editor.SetFont(self.fuente)
+		self.this_font = wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL)
+		self.this_font.SetFaceName("Courier New")
+		self.editor.SetFont(self.this_font)
 		self.editor.SetBackgroundStyle(True)
 	
 	def crearMenu(self):		
 		"""
-		Crea la barra de menú
+		Create menu bar
 		"""
-		marchivo=wx.Menu()
-		abrir=marchivo.Append(-1, "Abrir\tCtrl-O")
-		guardar=marchivo.Append(-1, "Guardar\tCtrl-S")
-		guardarComo=marchivo.Append(-1, "Guardar como")
+		mfile=wx.Menu()
+		open_m=mfile.Append(-1, "Open\tCtrl-O")
+		save_m=mfile.Append(-1, "Save\tCtrl-S")
+		saveAs=mfile.Append(-1, "Save as...")
 		
-		meditar=wx.Menu()
-		copiar=meditar.Append(-1, "Copiar\tCtrl-C")
-		pegar=meditar.Append(-1, "Pegar\tCtrl-V")
+		medit=wx.Menu()
+		copy=medit.Append(-1, "Copy\tCtrl-C")
+		paste=medit.Append(-1, "Paste\tCtrl-V")
 		
-		self.mtema=wx.Menu()
-		classic=self.mtema.Append(-1, "Classic")
-		dark=self.mtema.Append(-1, "Dark")
-		retro=self.mtema.Append(-1, "Retro")
-		pink=self.mtema.Append(-1, "Pink")
+		self.mtheme=wx.Menu()
+		classic=self.mtheme.Append(-1, "Classic")
+		dark=self.mtheme.Append(-1, "Dark")
+		retro=self.mtheme.Append(-1, "Retro")
+		pink=self.mtheme.Append(-1, "Pink")
 		
 		mayuda=wx.Menu()
-		ayuda=mayuda.Append(-1, "Ayuda")
-		acerca=mayuda.Append(-1, "Acerca de...")
+		app_help=mayuda.Append(-1, "Help")
+		about=mayuda.Append(-1, "About...")
 		
-		barraMenu=wx.MenuBar()
-		barraMenu.Append(marchivo, "Archivo")
-		barraMenu.Append(meditar, "Editar")
-		barraMenu.Append(self.mtema, "Seleccionar tema")
-		barraMenu.Append(mayuda, "Ayuda")
-		self.SetMenuBar(barraMenu)
+		menu_bar = wx.MenuBar()
+		menu_bar.Append(mfile, "File")
+		menu_bar.Append(medit, "Edit")
+		menu_bar.Append(self.mtheme, "Select theme")
+		menu_bar.Append(mayuda, "Help")
+		self.SetMenuBar(menu_bar)
 		
-		# Definición de "eventos"
-		self.Bind(wx.EVT_MENU, self.abrirArchivo, abrir)
-		self.Bind(wx.EVT_MENU, self.guardarArchivoComo, guardarComo)
-		self.Bind(wx.EVT_MENU, self.guardarArchivo, guardar)
+		# Bind events
+		self.Bind(wx.EVT_MENU, self.openFile, open_m)
+		self.Bind(wx.EVT_MENU, self.saveAs, saveAs)
+		self.Bind(wx.EVT_MENU, self.saveFile, save_m)
 		
-		self.Bind(wx.EVT_MENU, self.copiar, copiar)
-		self.Bind(wx.EVT_MENU, self.pegar, pegar)
+		self.Bind(wx.EVT_MENU, self.copy, copy)
+		self.Bind(wx.EVT_MENU, self.paste, paste)
 		
-		self.Bind(wx.EVT_MENU, self.configurarTema, classic)
-		self.Bind(wx.EVT_MENU, self.configurarTema, dark)
-		self.Bind(wx.EVT_MENU, self.configurarTema, retro)
-		self.Bind(wx.EVT_MENU, self.configurarTema, pink)
+		self.Bind(wx.EVT_MENU, self.setTheme, classic)
+		self.Bind(wx.EVT_MENU, self.setTheme, dark)
+		self.Bind(wx.EVT_MENU, self.setTheme, retro)
+		self.Bind(wx.EVT_MENU, self.setTheme, pink)
 		
-		self.Bind(wx.EVT_MENU, self.acerca, acerca)
-		self.Bind(wx.EVT_MENU, self.ayuda, ayuda)
+		self.Bind(wx.EVT_MENU, self.about, about)
+		self.Bind(wx.EVT_MENU, self.app_help, app_help)
 	
-	def abrirArchivo(self, event):
-		dlg=wx.FileDialog(self, "Abrir archivo", os.getcwd(), style=wx.OPEN)
+	def openFile(self, event):
+		dlg=wx.FileDialog(self, "Open file", os.getcwd(), style=wx.OPEN)
 		if dlg.ShowModal() == wx.ID_OK:
 			try:
 				fid=open(dlg.GetPath(),'r')
-				texto=fid.readlines()
-				self.texto="".join(texto)
-				self.texto = self.texto.decode("utf8")
+				text=fid.readlines()
+				self.text="".join(text)
+				self.text = self.text.decode("utf8")
 				fid.close()
-				self.editor.SetValue(self.texto)
-				self.archivo=dlg.GetPath()
-				self.SetTitle("MiniEditor "+self.archivo)
+				self.editor.SetValue(self.text)
+				self.this_file=dlg.GetPath()
+				self.SetTitle("MiniEditor "+self.this_file)
 			except:
-				wx.MessageBox(u"Archivo no válido","Error")
+				wx.MessageBox(u"Invalid file", "Error")
 		dlg.Destroy()
 	
-	def guardarArchivoComo(self, event):
+	def saveAs(self, event):
 		"""
-		Guarda el archivo actual abriendo un cuadro de diálogo
+		Save file as... 
 		"""
-		dlg=wx.FileDialog(self, "Guardar", os.getcwd(), style=wx.SAVE)
+		dlg=wx.FileDialog(self, "Save", os.getcwd(), style=wx.SAVE)
 		if dlg.ShowModal() == wx.ID_OK:
 			fid=open(dlg.GetPath(),'w')
 			txt=str(self.editor.GetValue().encode('utf8'))
 			fid.write(txt)
 			fid.close()
-			self.archivo=dlg.GetPath()
+			self.this_file=dlg.GetPath()
 		dlg.Destroy()
 		
-	def guardarArchivo(self,event):
+	def saveFile(self,event):
 		"""
-		Guarda el archivo actual 
+		Save current file
 		"""
-		if hasattr(self, 'archivo'):
-			fid=open(self.archivo,'w')
+		if hasattr(self, 'this_file'):
+			fid=open(self.this_file,'w')
 			txt=str(self.editor.GetValue().encode('utf8'))
 			fid.write(txt)
 			fid.close()
-			wx.MessageBox("Hecho","MiniEditor")
+			wx.MessageBox("Done...","MiniEditor")
 		else:
-			self.guardarArchivoComo(None)
+			self.saveAs(None)
 	
-	def copiar(self,event):
+	def copy(self,event):
 		"""
-		Copia el texto seleccionado al portapapeles
+		Copy selected text to the clipboard
 		"""
-		texto=wx.TextDataObject(self.editor.GetStringSelection())
+		text=wx.TextDataObject(self.editor.GetStringSelection())
 		if wx.TheClipboard.Open():
-			wx.TheClipboard.SetData(texto)
+			wx.TheClipboard.SetData(text)
 			wx.TheClipboard.Close()
 			
-	def pegar(self,event):
+	def paste(self,event):
 		"""
-		Pega el texto ubicado en el portapapeles
+		Paste the text contained in the clipboard
 		"""
 		txt=wx.TextDataObject()
 		if wx.TheClipboard.Open():
@@ -154,35 +155,34 @@ class MiniEditor(wx.Frame):
 			self.editor.write(txt.GetText())
 	
 		
-	def configurarTema(self,event):
+	def setTheme(self,event):
 		"""
-		Modifica el color de fondo y fuente del editor.
+        Update background and font color
 		"""
-		tema_sel=self.mtema.FindItemById(event.GetId()).GetText()
-		temas={'Classic':((0,0,255),(255,255,255)),
+		sel_theme=self.mtheme.FindItemById(event.GetId()).GetText()
+		themes = {'Classic':((0,0,255),(255,255,255)),
 		'Dark':((200,200,200),(0,0,0)),
 		'Retro':((0,255,0),(0,0,0)),
 		'Pink':((20,50,50),(250,180,180))}
-		self.editor.SetForegroundColour(temas[tema_sel][0])
-		self.editor.SetBackgroundColour(temas[tema_sel][1])
+		self.editor.SetForegroundColour(themes[sel_theme][0])
+		self.editor.SetBackgroundColour(themes[sel_theme][1])
 		self.editor.Refresh()
 		
-	def ayuda(self,event):
-		wx.MessageBox("No disponible","MiniEditor")
+	def app_help(self,event):
+		wx.MessageBox("Unavailable","MiniEditor")
 		
-	def acerca(self, event):
+	def about(self, event):
 		"""
-		Muestra un cuadro de diálogo con información 
-		básica acerca de la aplicación.
+		Show info about app
 		"""
-		descripcion="Editor de texto sin formato desarrollado en wxPython"
+		description="Plain text editor developed using wxPython"
 		info=wx.AboutDialogInfo()
 		info.SetName('MiniEditor')
-		info.SetDescription(descripcion)
+		info.SetDescription(description)
 		info.SetVersion('0.1.0')
 		info.SetLicense('MIT License')
 		info.SetDevelopers(['Pedro Jorge De Los Santos'])
-		info.SetWebSite(('labdls.blogspot.mx','LAB DLS'))
+		info.SetWebSite(('codigo-wxpython.blogspot.mx',u'Código wxPython'))
 		info.SetCopyright('(c) 2016')
 		wx.AboutBox(info)
 
